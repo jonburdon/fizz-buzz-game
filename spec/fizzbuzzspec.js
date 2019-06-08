1133d1e6f13ef0a9e5d8e42884db495d9328da8e
Ubuntu <ubuntu@ip-172-31-95-8.ec2.internal>
2019-06-08 14:22:21 +0000

    Checks succesfully for numbers which do not return Fizz, Buzz or FizzBuzz

--- spec/fizzbuzzspec.js
+++ spec/fizzbuzzspec.js
@@ -18,9 +18,23 @@ describe("Fizz Buzz Game", function() {
     });
 
        describe("Checks fb input", function() {
-        it("should return Fizz", function() {
+        it("should return buzz", function() {
             var result = fizzBuzzGame(5)
             expect(result).toBe("Buzz");
+        });
+    });
+    
+           describe("Checks fb input", function() {
+        it("should return FizzBuzz", function() {
+            var result = fizzBuzzGame(15)
+            expect(result).toBe("FizzBuzz");
+        });
+    });
+    
+               describe("Checks fb input", function() {
+        it("should return 1", function() {
+            var result = fizzBuzzGame(1)
+            expect(result).toBe(1);
         });
     });
 
